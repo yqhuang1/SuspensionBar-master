ff64a0366dd0a5792705e7c8d477cdda5d87cbbb
yqhuang1 <yqhuang1@qq.com>
2019-07-17 23:52:06 +0800

    从Android Studio检测Git

---
+++ app/src/main/java/com/xiaopo/flying/suspensionbar/RestrictActivity.java
@@ -0,0 +1,13 @@
+package com.xiaopo.flying.suspensionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestrictActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restrict);
+    }
+}
